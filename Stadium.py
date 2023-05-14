bd3e5ebc3f546d2a297dd7acdbbfd5447b52524c
Ivan Lerner <ivanlerner47@gmail.com>
2023-05-14 14:00:09 +0300

    seter convention fixed

--- Stadium.py
+++ Stadium.py
@@ -1,39 +1,51 @@
 class Stadium:
 
-    def __init__(self, name, capacity, current_attandies, home_team, away_team):
-        self.name = name
-        self.capacity = capacity
-        self.current_attandies = current_attandies
-        self.home_team = home_team
-        self.away_team = away_team
+    def __init__(self, __name, __capacity, __current_attandies, __home_team, __away_team):
+        self.__name = __name
+        self.__capacity = __capacity
+        self.__current_attandies = __current_attandies
+        self.__home_team = __home_team
+        self.__away_team = __away_team
+        @property
+        def model(self):
+            return self.__model
+        @model.setter
+        def model(self,__name):
+            return self.__model
 
-    def get_name(self):
-        return self.name
-    def get_capacity(self):
-        return self.capacity
-    def get_current_attandies(self):
-        return self.current_attandies
-    def get_home_team(self):
-        return self.home_team
-    def get_away_team(self):
-        return self.away_team
+        @model.setter
+        def model(self, __capacity):
+            return self.__model@model.setter
+        @model.setter
+        def model(self,__current_attandies):
+            return self.__model
+
+        @model.setter
+        def model(self, __home_team):
+            return self.__model
+
+        @model.setter
+        def model(self, __away_team):
+            return self.__model
+
+
 
     def add_attandies(self, count):
-        self.current_attandies += count
-        if self.current_attandies > self.capacity:
-            excess = self.current_attandies - self.capacity
-            self.current_attandies = self.capacity
-            print("замало місць на стадіоні, надлишок:", excess,".", "кількість глядачів змінено на ", self.current_attandies)
+        self.__current_attandies += count
+        if self.__current_attandies > self.__capacity:
+            excess = self.__current_attandies - self.__capacity
+            self.__current_attandies = self.__capacity
+            print("замало місць на стадіоні, надлишок:", excess,".", "кількість глядачів змінено на ", self.__current_attandies)
 
     def decrease_attendance(self):
-        self.current_attandies -= 100
-        if self.current_attandies < 0:
-            self.current_attandies = 0
+        self.__current_attandies -= 100
+        if self.__current_attandies < 0:
+            self.__current_attandies = 0
             print("кількість глядачів не може бути відємною, кількість глядачів змінена на 0")
 
-    def change_home_team(self, team_name):
-        self.home_team = team_name
+    def change___home_team(self, team_name):
+        self.__home_team = team_name
 
-    def change_away_team(self, team_name):
-        self.away_team = team_name
+    def change___away_team(self, team_name):
+        self.__away_team = team_name
 
